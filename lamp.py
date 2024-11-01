7b1ab65ee1b095f34c6c9edbea684a350d43492e
Mahdi_Ghanbarpour <mahdi10ghanbarpour@gmail.com>
2024-11-01 01:08:48 -0700

    Update lamp.py

--- lamp.py
+++ lamp.py
@@ -1,17 +1,17 @@
-def count(n,s):
-    ps = s [0]
-    c= 0
-    for i in range(1,n):
-        cs= s[1]
-        cs !=ps
-        c+=1
-        ps = cs
-    return c
-n = int(input())
-s= [int(input())for i in range(n)]
-print(count(n,s))
-n= int(input())
-s= int(input())
+# def count(n,s):
+#     ps = s [0]
+#     c= 0
+#     for i in range(1,n):
+#         cs= s[1]
+#         cs !=ps
+#         c+=1
+#         ps = cs
+#     return c
+# n = int(input())
+# s= [int(input())for i in range(n)]
+# print(count(n,s))
+# n= int(input())
+# s= int(input())
 
 
 
